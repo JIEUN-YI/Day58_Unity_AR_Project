a45e6e2e211ffdb543053012351921af42e08ad3
YIJEUN <yijeun.dev@gmail.com>
2024-10-09 22:28:47 +0900

    [feat]UI의 기능 구현 + 총의 발사 구현
    1. UI버튼들의 기능을 각 필요에 맞게 구현
     - 메뉴버튼 활성화
     - 게임 종료 버튼 구현
     - 발사버튼과 재장전 버튼 구현
     - 현재 탄환을 다 사용한 경우 재장전 메세지 출력
    
    2. 총의 발사 효과 구현
     - 발사버튼 터치 시 발사
     - 재장전 버튼 터치 시 현재 탄환 수 회복
     - 몬스터와 충돌 시 충돌 효과 발생

---
+++ Assets/ShooterController.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ShooterController : MonoBehaviour
+{
+    [SerializeField] Transform fireCamPoint; // 레이저 시작 지점 = 카메라
+    public float damange = 100;
+    public float range = 30f; // 사정 거리
+    [SerializeField] LayerMask layerMask; // 부딪힐 충돌체 레이어 설정
+    [SerializeField] private GameObject impactEffect; // 적 피격 파티클
+
+    [Header("Magzine")]
+    [SerializeField] public int curMagzine; // 현재 탄창수
+    [SerializeField] public int maxMagazine; // 최대 탄창수
+    public bool isReload = false; // 재장전 판단여부
+    private void Start()
+    {
+        fireCamPoint = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
+        curMagzine = maxMagazine;
+    }
+
+    private void Update()
+    {
+        Debug.DrawRay(fireCamPoint.position, fireCamPoint.forward * range, Color.red); // 레이캐스트를 보여주는 함수
+    }
+
+    public void Shoot()
+    {
+        Ray ray = new Ray(fireCamPoint.position, fireCamPoint.forward * range);
+        RaycastHit hit;
+        curMagzine--; // 발사시 현재 탄환 수 감소
+        
+        if (curMagzine > 0) // 탄환수가 0 이상이면
+        {
+            if (Physics.Raycast(ray, out hit, range))
+            {
+                Debug.Log(" 맞았다. ");
+                Debug.Log($" {hit.collider.name} ");
+                StartCoroutine(OnFlashEffect(hit));
+            }
+        }
+        else if(curMagzine <= 0)
+        {
+            Debug.Log("재장전이 필요합니다.");
+            curMagzine = 0;
+            isReload = true; // 재장전 필요
+        }
+    }
+
+    public void Reload()
+    {
+        StartCoroutine(OnReload());
+        isReload = false;
+    }
+    /// <summary>
+    /// 총알 충돌 이펙트 발사 코루틴
+    /// </summary>
+    /// <param name="hit"></param>
+    /// <returns></returns>
+    public IEnumerator OnFlashEffect(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(impactEffect, hit.point, Quaternion.identity); // hit 이펙트 발생
+        yield return new WaitForSeconds(0.2f);
+        Destroy(impact); // 0.2초후 삭제
+    }
+
+    /// <summary>
+    /// 총알 장전 코루틴
+    /// UI의 장전을 숫자가 올라가는 것이 보이게 생성되기 위해서 코루틴으로 설정
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator OnReload()
+    {
+        while(curMagzine < maxMagazine)
+        {
+            curMagzine++;
+            yield return new WaitForSeconds(0.05f);
+        }
+        curMagzine = maxMagazine;
+    }
+
+}
