9040e8b15dd0a9ebd67bd3ba65880c78b119ae15
YIJEUN <yijeun.dev@gmail.com>
2024-10-14 11:53:51 +0900

    [Feat] 몬스터의 위치 생성 기능 추가
    몬스터의 위치를 랜덤하게 출력하도록 추가

--- Assets/MonsterController.cs
+++ Assets/MonsterController.cs
@@ -21,7 +21,8 @@ public class MonsterController : MonoBehaviour
     [Header("Raycast")]
     [SerializeField] Transform monsterPoint; // 바닥으로 발사할 레이캐스트의 시작점
     private float range = 5f;
-    /* 기본 몬스터 출력법 */
+
+    /* 기본 몬스터 출력법 
         private void Awake()
         {
             isMonster = false;
@@ -71,215 +72,99 @@ public class MonsterController : MonoBehaviour
             yield return new WaitForSeconds(3f);
             isMonster = false;
         }
-        
-    /* 바닥인식을 통한 출력
-    private void Update()
-    {
-        Debug.Log("게임시작");
-        if (SelectPlane() != null)
-        {
-            if (isMonster == false) // 몬스터가 없으면
-            {
-                Debug.Log("몬스터가 없으면");
-                MonsterCreate(); // 몬스터 생성
-                isMonster = true; // 몬스터가 있다고 표현
-            }
-            else if (isMonster == true) // 몬스터가 있으면
-            {
-                if (curMonsterHp <= 0) // 체력이 0 이하일 때
-                {
-                    StartCoroutine(MonsterDied()); // 삭제 코루틴 시작
-                }
-            }
-        }
-        Debug.Log("바닥인식안됨");
-
-    }
-    private void MonsterCreate()
-    {
-        RaycastHit hit;
-        ARPlane plane = SelectPlane(); // 바닥 하나를 무작위로 선정
-        Vector3 center = plane.center; // 바닥의 가운데 지점
-        Vector2 size = plane.size; // 바닥의 사각형 사이즈 (x, y) 사이즈
-        Vector3 monsterPos; // 몬스터가 출력될 위치
-        Debug.Log("몬스터의 생성 시작");
-
-        while (true)
-        {
-            // 몬스터의 위치를 랜덤으로 지정
-            monsterPos
-                = new Vector3(center.x + Random.Range(-size.x * 0.4f, -size.x * 0.4f), // 바닥의 사이즈 안에서 랜덤으로 
-                              center.y,
-                              center.z + Random.Range(-size.x * 0.4f, -size.x * 0.4f));
-            Debug.Log("몬스터의 랜덤 위치 출력");
-            // 1. 바닥이 없는 경우에는 몬스터의 위치를 다시 지정
-            // 몬스터의 위치보다 높은 위치에서, 아래방향으로, 충돌체를, 지정범위안에, PlaneMask레이어층에 있는 충돌체 확인
-            if (Physics.Raycast(monsterPos + Vector3.up, Vector3.down, out hit, range, PlaneMask) == false)
-            {
-                Debug.Log("바닥이 없어서 몬스터 위치 다시 출력");
-                continue;
-            }
-            // 2. 바닥의 위치가 벽 뒤에 있는 경우 몬스터의 위치를 다시 지정
-            // 플레이어의 시야 위치에서, 몬스터의 지정위치보다 조금 위의 위치한, PlaneMask레이어에 해당하는 충돌확인
-            if (Physics.Linecast(MainCamPoint.transform.position, monsterPos + Vector3.up * 0.1f, PlaneMask))
-            {
-                Debug.Log("벽으로 막혀서 몬스터 위치 다시 출력");
-                continue;
-            }
-            break;
-        }
-        CreateMonster(monsterPos);
-        Debug.Log("몬스터의 생성 완료");
-
-    }
-
-    private ARPlane SelectPlane()
-    {
-        Debug.Log("바닥 선정 시작");
-        List<ARPlane> horizontalPlane = new List<ARPlane>(); // ARPlane을 가지는 리스트 생성
-        // PlanManager에서 바닥면인 Plane을 전부 리스트에 포함
-        foreach (ARPlane plane in planeManager.trackables)
-        {
-            // 만약 Plane의 선 종류가 Horizontal위의 있는 경우
-            if (plane.alignment == UnityEngine.XR.ARSubsystems.PlaneAlignment.HorizontalUp)
-            {
-                Debug.Log("바닥 리스트 추가");
-                horizontalPlane.Add(plane); // 리스트에 추가
-            }
-        }
-        if (horizontalPlane.Count == 0)
-        {
-            Debug.Log("null 반환");
-            return null;
-        }
-        Debug.Log("바닥 반환");
-        // 리스트 안의 plane을 랜덤하게 반환
-        return horizontalPlane[Random.Range(0, horizontalPlane.Count)];
-    }
-
-    private void CreateMonster(Vector3 monsterPos)
-    {
-        nowMonster = Instantiate(monsterPrefab, monsterPos, Quaternion.LookRotation(MainCamPoint.transform.position));
-        isMonster = true;
-    }
-
-    /// <summary>
-    /// 몬스터의 삭제를 표현하는 코루틴
-    /// 삭제 후 새로 생성까지의 시간을 조절
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator MonsterDied()
-    {
-        Debug.Log("코루틴");
-        Destroy(nowMonster);
-        curMonsterHp = maxMonsterHp;
-        yield return new WaitForSeconds(5f);
-        isMonster = false;
-    }
-    /* 바닥인식을 통한 출력 - 기본 / 에러발생
-    private void Update()
-    {
-
-        Debug.Log("게임시작");
-
-        if (SelectPlane() != null)
-        {
-            if (isMonster == false) // 몬스터가 없으면
-            {
-                Debug.Log("몬스터가 없으면");
-                MonsterCreate(); // 몬스터 생성
-                isMonster = true; // 몬스터가 있다고 표현
-            }
-            else if (isMonster == true) // 몬스터가 있으면
-            {
-                if (curMonsterHp <= 0) // 체력이 0 이하일 때
-                {
-                    StartCoroutine(MonsterDied()); // 삭제 코루틴 시작
-                }
-            }
-        }
-        Debug.Log("바닥인식안됨");
-    }
-    private void MonsterCreate()
-    {
-        RaycastHit hit;
-        ARPlane plane = SelectPlane(); // 바닥 하나를 무작위로 선정
-        Vector3 center = plane.center; // 바닥의 가운데 지점
-        Vector2 size = plane.size; // 바닥의 사각형 사이즈 (x, y) 사이즈
-        Vector3 monsterPos; // 몬스터가 출력될 위치
-        Debug.Log("몬스터의 생성 시작");
-
-        while (true)
-        {
-            // 몬스터의 위치를 랜덤으로 지정
-            monsterPos
-                = new Vector3(center.x + Random.Range(-size.x * 0.4f, -size.x * 0.4f), // 바닥의 사이즈 안에서 랜덤으로 
-                              center.y,
-                              center.z + Random.Range(-size.x * 0.4f, -size.x * 0.4f));
-            Debug.Log("몬스터의 랜덤 위치 출력");
-            // 1. 바닥이 없는 경우에는 몬스터의 위치를 다시 지정
-            // 몬스터의 위치보다 높은 위치에서, 아래방향으로, 충돌체를, 지정범위안에, PlaneMask레이어층에 있는 충돌체 확인
-            if (Physics.Raycast(monsterPos + Vector3.up, Vector3.down, out hit, range, PlaneMask) == false)
-            {
-                Debug.Log("바닥이 없어서 몬스터 위치 다시 출력");
-                continue;
-            }
-            // 2. 바닥의 위치가 벽 뒤에 있는 경우 몬스터의 위치를 다시 지정
-            // 플레이어의 시야 위치에서, 몬스터의 지정위치보다 조금 위의 위치한, PlaneMask레이어에 해당하는 충돌확인
-            if (Physics.Linecast(MainCamPoint.transform.position, monsterPos + Vector3.up * 0.1f, PlaneMask))
-            {
-                Debug.Log("벽으로 막혀서 몬스터 위치 다시 출력");
-                continue;
-            }
-            break;
-        }
-        CreateMonster(monsterPos);
-        Debug.Log("몬스터의 생성 완료");
-
-    }
-
-    private ARPlane SelectPlane()
-    {
-        Debug.Log("바닥 선정 시작");
-        List<ARPlane> horizontalPlane = new List<ARPlane>(); // ARPlane을 가지는 리스트 생성
-        // PlanManager에서 바닥면인 Plane을 전부 리스트에 포함
-        foreach (ARPlane plane in planeManager.trackables)
-        {
-            // 만약 Plane의 선 종류가 Horizontal위의 있는 경우
-            if (plane.alignment == UnityEngine.XR.ARSubsystems.PlaneAlignment.HorizontalUp)
-            {
-                Debug.Log("바닥 리스트 추가");
-                horizontalPlane.Add(plane); // 리스트에 추가
-            }
-        }
-        if (horizontalPlane.Count == 0)
-        {
-            Debug.Log("null 반환");
-            return null;
-        }
-        Debug.Log("바닥 반환");
-        // 리스트 안의 plane을 랜덤하게 반환
-        return horizontalPlane[Random.Range(0, horizontalPlane.Count)];
-    }
-
-    private void CreateMonster(Vector3 monsterPos)
-    {
-        nowMonster = Instantiate(monsterPrefab, monsterPos, Quaternion.LookRotation(MainCamPoint.transform.position));
-        isMonster = true;
-    }
-
-    /// <summary>
-    /// 몬스터의 삭제를 표현하는 코루틴
-    /// 삭제 후 새로 생성까지의 시간을 조절
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator MonsterDied()
-    {
-        Debug.Log("코루틴");
-        Destroy(nowMonster);
-        curMonsterHp = maxMonsterHp;
-        yield return new WaitForSeconds(5f);
-        isMonster = false;
-    }
     */
+    /* 바닥인식을 통한 출력*/
+
+    private void Awake()
+    {
+        isMonster = false;
+    }
+
+    private void Update()
+    {
+        Debug.Log("게임시작");
+        if (SelectPlane() != null)
+        {
+            if (isMonster == false) // 몬스터가 없으면
+            {
+                MonsterCreate(); // 몬스터 생성
+            }
+            else if (isMonster == true) // 몬스터가 있으면
+            {
+                if (curMonsterHp <= 0) // 체력이 0 이하일 때
+                {
+                    StartCoroutine(MonsterDied()); // 삭제 코루틴 시작
+                }
+            }
+        }
+    }
+    private void MonsterCreate()
+    {
+        RaycastHit hit;
+        ARPlane plane = SelectPlane(); // 바닥 하나를 무작위로 선정
+        Vector3 center = plane.center; // 바닥의 가운데 지점
+        Vector2 size = plane.size; // 바닥의 사각형 사이즈 (x, y) 사이즈
+        Vector3 monsterPos; // 몬스터가 출력될 위치
+
+        // 몬스터의 위치를 랜덤으로 지정
+        monsterPos
+            = new Vector3(center.x + Random.Range(-size.x * 0.4f, size.x * 0.4f), // 바닥의 사이즈 안에서 랜덤으로 
+                          center.y,
+                          center.z + Random.Range(-size.y * 0.4f, size.y * 0.4f));
+        // 1. 바닥이 없는 경우에는 함수 종료 => 다시 바닥을 탐색
+        // 몬스터의 위치보다 높은 위치에서, 아래방향으로, 충돌체를, 지정범위안에, PlaneMask레이어층에 있는 충돌체 확인
+        if (Physics.Raycast(monsterPos + Vector3.up, Vector3.down, out hit, range, PlaneMask) == false)
+        {
+            return;
+        }
+        // 2. 바닥의 위치가 벽 뒤에 있는 경우 함수 종료 => 다시 바닥을 탐색
+        // 플레이어의 시야 위치에서, 몬스터의 지정위치보다 조금 위의 위치한, PlaneMask레이어에 해당하는 충돌확인
+        if (Physics.Linecast(MainCamPoint.transform.position, monsterPos + Vector3.up * 0.1f, PlaneMask))
+        {
+            return;
+        }
+        CreateMonster(monsterPos);
+        isMonster = true; // 몬스터는 하나만 생성되므로 제어
+
+    }
+
+    private ARPlane SelectPlane()
+    {
+        List<ARPlane> horizontalPlane = new List<ARPlane>(); // ARPlane을 가지는 리스트 생성
+        // PlanManager에서 바닥면인 Plane을 전부 리스트에 포함
+        foreach (ARPlane plane in planeManager.trackables)
+        {
+            // 만약 Plane의 선 종류가 Horizontal위의 있는 경우
+            if (plane.alignment == UnityEngine.XR.ARSubsystems.PlaneAlignment.HorizontalUp)
+            {
+                horizontalPlane.Add(plane); // 리스트에 추가
+            }
+        }
+        if (horizontalPlane.Count == 0)
+        {
+            return null;
+        }
+        // 리스트 안의 plane을 랜덤하게 반환
+        return horizontalPlane[Random.Range(0, horizontalPlane.Count)];
+    }
+
+    private void CreateMonster(Vector3 monsterPos)
+    {
+        nowMonster = Instantiate(monsterPrefab, monsterPos, Quaternion.LookRotation(MainCamPoint.transform.position));
+        isMonster = true;
+    }
+
+    /// <summary>
+    /// 몬스터의 삭제를 표현하는 코루틴
+    /// 삭제 후 새로 생성까지의 시간을 조절
+    /// </summary>
+    /// <returns></returns>
+    private IEnumerator MonsterDied()
+    {
+        Destroy(nowMonster);
+        curMonsterHp = maxMonsterHp;
+        yield return new WaitForSeconds(5f); // 새로운 몬스터 생성까지의 시간을 제어
+        isMonster = false;
+    }
+
 }
